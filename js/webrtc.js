e0cf1734d4e2995b235ac0a080020eb8c4a3a27b
PrincessMelime <ingenieria.mel@gmail.com>
2017-06-15 13:52:56 -0300

    Create webrtc.js

--- js/webrtc.js
+++ js/webrtc.js
@@ -56,7 +56,7 @@ var PHONE = window.PHONE = function(config) {
     // STUN Server List Configuration (public STUN list)
     //{"url":"turn:35.161.239.237:13478","credential":"concrema","username":"flan"}
    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    var rtcconfig = { iceServers :  [{"url":"turn:35.161.239.237","credential":"concrema","username":"flan"}]};
+    var rtcconfig = { iceServers :  [{"url":"turn:35.161.239.237","credential":"meli","username":"melisa"}]};
     
   //  	{ "url" :
   //      navigator.mozGetUserMedia    ? "stun:stun.services.mozilla.com" :
